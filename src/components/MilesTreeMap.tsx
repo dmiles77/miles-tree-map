8629958de674e1aba1db2a82ae643ef48a2af7b4
unknown <danielmiles89@gmail.com>
2025-02-14 18:00:15 +0200

    fixes

--- src/components/MilesTreeMap.tsx
+++ src/components/MilesTreeMap.tsx
@@ -1,49 +1,5 @@
-import React from 'react';
-
-interface TreeNode {
-  id: string;
-  name: string;
-  value: number;
-  children?: TreeNode[];
-}
-
-interface MilesTreeMapProps {
-  data: TreeNode;
-  renderComponent?: (node: TreeNode) => React.ReactElement;
-  colorRange?: [string, string];
-  onNodeClick?: (node: TreeNode) => void;
-  animationDuration?: number;
-  breadcrumbEnabled?: boolean;
-  backButtonEnabled?: boolean;
-  backButtonPosition?: 'top-left' | 'top-right';
-  padding?: number;
-  borderRadius?: number;
-  minDisplayValue?: number;
-  defaultZoomLevel?: number;
-  fontSizeScale?: number;
-  tooltipEnabled?: boolean;
-  minCustomComponentSize?: number;
-}
-
-const MilesTreeMap: React.FC<MilesTreeMapProps> = ({
-  data,
-  renderComponent,
-  colorRange = ['#ff0000', '#00ff00'],
-  onNodeClick,
-  animationDuration = 300,
-  breadcrumbEnabled = true,
-  backButtonEnabled = true,
-  backButtonPosition = 'top-left',
-  padding = 5,
-  borderRadius = 5,
-  minDisplayValue = 0,
-  defaultZoomLevel = 1,
-  fontSizeScale = 1,
-  tooltipEnabled = true,
-  minCustomComponentSize = 50,
-}) => {
-  // Component implementation here
-  return <div>hello</div>;
+const MilesTreeMap = () => {
+  return <div>Hello</div>;
 };
 
 export default MilesTreeMap;
