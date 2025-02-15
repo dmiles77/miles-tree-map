b65c6d0b8256ccb3a6cde8290daefd06ce8472b5
unknown <danielmiles89@gmail.com>
2025-02-15 10:00:15 +0200

    fix

---
+++ src/components/MilesTreeMap.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const MilesTreeMap = () => {
+  return <div>Hello World!</div>
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-export const MilesTreeMap = () => {
-    return "Hello World!"
-}
+export { MilesTreeMap } from './components/MilesTreeMap';
